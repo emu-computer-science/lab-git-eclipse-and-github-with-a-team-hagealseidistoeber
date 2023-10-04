4f267d14cb2b097a27f0d3c83b9fe4b2e9fd731e
hadihage <145401012+hadihage@users.noreply.github.com>
2023-10-04 18:47:35 -0400

    changed glad in my own branch

--- gitProject/src/Glad.java
+++ gitProject/src/Glad.java
@@ -3,5 +3,12 @@ public class Glad {
 
 	public static void glad() {
         System.out.println("I'm feeling glad!");
+   
     }
+	
+	public static void him() {
+        System.out.println("I'm Him boii !");
+
+	}
+    
 }
